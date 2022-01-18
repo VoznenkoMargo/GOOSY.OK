0288d6a994321bad109457e9ea53ac95c90fec04
MichaelKostroma <mikbingo2018@gmail.com>
2022-01-18 20:27:14 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <h1>Привет,мир!</h1>
+      <h1>Hello world!</h1>
     </div>
   );
 }
