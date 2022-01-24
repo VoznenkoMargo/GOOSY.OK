be9d43d6e7b735decfdb7b1a895cc160d5be4da5
Marina <marinazhukova28@gmail.com>
2022-01-24 22:27:00 +0200

    add header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from "react";
 import "./App.scss";
 import { Provider } from "react-redux";
+import {BrowserRouter} from "react-router-dom";
 import Carousel from "./components/carousel/Carousel";
 import Item from "./components/Item/Item";
+import Header from "./components/Header/Header";
 import store from "./store";
 
 
 function App() {
   return (
       <Provider store={store}>
+          <BrowserRouter>
           <div className="App">
+                <Header />
+                <section>
               <Carousel />
               <Item/>
+              </section>
           </div>
+          </BrowserRouter>
       </Provider>
   );
 }
