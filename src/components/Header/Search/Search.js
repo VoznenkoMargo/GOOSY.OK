1389b69959d518cca6d684c2ae6170ce3a6c4aaf
Marina <marinazhukova28@gmail.com>
2022-01-27 01:18:54 +0200

    fixed header

--- src/components/Header/Search/Search.js
+++ src/components/Header/Search/Search.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { FiSearch } from "react-icons/fi";
 
 function Search() {
   return (
     <div>
       <form>
         <input type="text" placeholder="What would you like to eat?" />
+        <FiSearch/>
+        
       </form>
     </div>
   );
