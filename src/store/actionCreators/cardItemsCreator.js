ec6ab0ebe9ecd064eb4d589ed55df3d8a29de465
Andrey Vorona <voronaandrey95@gmail.com>
2022-02-08 03:00:55 +0200

    sd

--- src/store/actionCreators/cardItemsCreator.js
+++ src/store/actionCreators/cardItemsCreator.js
@@ -12,43 +12,20 @@ export const initCardItemsCreator = () => async (dispatch) => {
 };
 
 export const initСategoriesItemsCreator = (inquiry) => async (dispatch) => {
-  const { categories: categories, price: price } = inquiry;
-  const categ = `categories=${categories.toString()}`;
-  const pric = `minPrice=${price[0]}&maxPrice=${price[1]}`;
-  const allCategories = {};
-  if (categories.length !== 0) {
-    const {
-      data: { products },
-    } = await getByCategory(`${categ}&${pric}`);
-    products.forEach((item) => {
-      if (!(`${item.categories}` in allCategories)) {
-        allCategories[item.categories] = [item];
-      } else {
-        allCategories[item.categories] = [
-          ...allCategories[item.categories],
-          item,
-        ];
-      }
+
+  const allCategories={};
+    console.log(inquiry);
+    const { data: {products} } = await getByCategory(inquiry);   
+    products.forEach((item)=>{
+      if(!(`${item.categories}` in allCategories)){
+        allCategories[item.categories] = [item]
+       }
+        else {
+          allCategories[item.categories] = [...allCategories[item.categories],item]
+        }
     });
-    dispatch({ type: GET_FIND_ITEMS, payload: allCategories });
-  } else {
-    const {
-      data: { products },
-    } = await getByCategory(pric);
-    products.forEach((item) => {
-      if (!(`${item.categories}` in allCategories)) {
-        allCategories[item.categories] = [item];
-      } else {
-        allCategories[item.categories] = [
-          ...allCategories[item.categories],
-          item,
-        ];
-      }
-    });
-    console.log(allCategories);
-    dispatch({ type: GET_FIND_ITEMS, payload: allCategories });
-  }
-};
+      dispatch({type: GET_FIND_ITEMS, payload: allCategories});
+    }
 
 // export const initItems = () => (dispatch) => {
 //   axios
