51bffb52d8c9723074508d1157fdcfe15b7066b8
olksii <alexeyyali@gmail.com>
2022-02-10 22:17:05 +0200

    aded

---
+++ src/components/FormLogin/FormLogin.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Formik } from "formik";
+import * as yup from yup;
+import './FormLogin.scss'
+
+
+function FormLogin () {
+    <div>
+        <form>
+            <input type='text' name='name' placeholder="Name" />
+        </form>
+    </div>
+}
+export default FormLogin;
