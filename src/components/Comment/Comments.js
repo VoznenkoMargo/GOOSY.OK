126c34391307605791a74dabc6f05b0de6ef9aaf
Marharyta Voznenko <ritavoznenko@gmail.com>
2022-03-30 17:56:19 +0300

    fixRita

--- src/components/Comment/Comments.js
+++ src/components/Comment/Comments.js
@@ -1,118 +1,196 @@
-import React, { useState, useEffect } from 'react'
+/* eslint-disable react/jsx-no-bind */
+/* eslint-disable no-underscore-dangle */
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
-import { useDispatch, useSelector } from 'react-redux';
-import { Button, Comment, Form  } from 'semantic-ui-react'
+import { useDispatch, useSelector } from "react-redux";
+import { Button, Comment, Form } from "semantic-ui-react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPencilAlt } from '@fortawesome/free-solid-svg-icons';
-import { addUserComment, deleteOneUserComments } from '../../store/actionCreators/commentItemsCreator';
-import {getAllUsersCommentOfProduct, getAllUsersComments} from "../../store/actionCreators/commentItemsCreator";
-import UserComment from '../UserComment/UserComment';
-import styles from './Comments.module.scss';
-import UserRating from '../Rating/Rating';
+import { faPencilAlt } from "@fortawesome/free-solid-svg-icons";
+import {
+  addUserComment,
+  // deleteOneUserComments,  getAllUsersCommentOfProduct,
+  getAllUsersComments,
+} from "../../store/actionCreators/commentItemsCreator";
 
+import UserComment from "../UserComment/UserComment";
+import styles from "./Comments.module.scss";
+import UserRating from "../Rating/Rating";
 
 function Comments(props) {
-    const {id, setFlag, flag} = props;
-const dispatch = useDispatch();
-const {comments} = useSelector(store => store.comment);
-const [text, setText] = useState("");
-const [comm, setComm] = useState([]);
-const [visibleMore, setvisibleMore] = useState(true);
-const [rank, setRank] = useState(5);
-const {thumb} = useSelector(store => store.comment);
+  const { id, setFlag, flag } = props;
+  const dispatch = useDispatch();
+  const { comments } = useSelector((store) => store.comment);
+  const [text, setText] = useState("");
+  const [comm, setComm] = useState([]);
+  const [visibleMore, setvisibleMore] = useState(true);
+  const [rank, setRank] = useState(5);
+  const { thumb } = useSelector((store) => store.comment);
 
-// Очистка коментов
-// function del() {
-//   comments.forEach(elem => {
-//     dispatch(deleteOneUserComments(elem._id))
-//   })
-// }
-// del()
+  // Очистка коментов
+  // function del() {
+  //   comments.forEach(elem => {
+  //     dispatch(deleteOneUserComments(elem._id))
+  //   })
+  // }
+  // del()
 
-function scrollToComment() {
-  window.scrollTo({
-    top: 500,
-    behavior: "smooth",
-  });
-}
+  function scrollToComment() {
+    window.scrollTo({
+      top: 500,
+      behavior: "smooth",
+    });
+  }
 
-const scrollToTopUnauth = () => {
-  window.scrollTo({
-    top: 0,
-    behavior: "instant",
-  });
-};
+  const scrollToTopUnauth = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "instant",
+    });
+  };
 
   function handleSubmit(event) {
     event.preventDefault();
     setText("");
   }
 
-const commentsFilter = comments.filter(elem => elem.product._id === id);
-  
-  function handleShowMoreComments(){
-      setComm(commentsFilter);
-      setvisibleMore(false);
+  const commentsFilter = comments.filter((elem) => elem.product._id === id);
+
+  function handleShowMoreComments() {
+    setComm(commentsFilter);
+    setvisibleMore(false);
   }
 
-const commentsSlice = commentsFilter.reverse().slice(0, 5);
+  const commentsSlice = commentsFilter.reverse().slice(0, 5);
 
-  useEffect(()=>{
-    if(!visibleMore){
+  useEffect(() => {
+    if (!visibleMore) {
       setComm(commentsFilter);
-      setRank(5)
-    }else{
-    setComm(commentsSlice);
-    setRank(5);
-    setvisibleMore(true);
-  }
-  },[comments]);
-
-  useEffect(()=>{
-    dispatch(getAllUsersComments()); 
-  },[thumb]);
-
-  function addComment(idProduct, textComment){
-    dispatch(addUserComment({product: `${idProduct}`, content: `${textComment}`, datePublic: Date.now(), rating:`${rank}`})); 
-    dispatch(getAllUsersComments()); 
-    setFlag(!flag); 
-    setvisibleMore(true);
-    if(localStorage.getItem("authToken")){
+      setRank(5);
+    } else {
       setComm(commentsSlice);
-      scrollToComment()}
-      else{
-        scrollToTopUnauth();
-      }
+      setRank(5);
+      setvisibleMore(true);
+    }
+  }, [comments]);
+
+  useEffect(() => {
+    dispatch(getAllUsersComments());
+  }, [thumb]);
+
+  function addComment(idProduct, textComment) {
+    dispatch(
+      addUserComment({
+        product: `${idProduct}`,
+        content: `${textComment}`,
+        datePublic: Date.now(),
+        rating: `${rank}`,
+      })
+    );
+    dispatch(getAllUsersComments());
+    setFlag(!flag);
+    setvisibleMore(true);
+    if (localStorage.getItem("authToken")) {
+      setComm(commentsSlice);
+      scrollToComment();
+    } else {
+      scrollToTopUnauth();
+    }
   }
 
   return (
-    <Comment.Group style={{paddingTop: "20px"}}>
-    <Comment style={{padding: '10px 0px 10px 0px', maxWidth: "500px"}}>
-  {commentsFilter.length > 0 
-  ? comm.map(elem => <UserComment key={elem._id} commentsId={elem._id} firstName={elem.customer.firstName} content={elem.content} date={elem.datePublic} avatar={elem.customer.avatarUrl} rating={elem.rating} thumbUpUser={elem.thumbUp === undefined ? 0 : elem.thumbUp} thumbDownUser={elem.thumbDown === undefined ? 0 : elem.thumbDown} /> ) 
-  : <p style={{fontSize:"18px", color: "rgb(241, 241, 241)" , padding: "20px 0px 20px 20px", backgroundColor: "#4e4948", borderRadius: "10px"}}>No comments</p> } 
-    </Comment>
-    {commentsFilter.length > 5 && visibleMore
-    ? <Comment.Text onClick={handleShowMoreComments} style={{marginBottom: "20px", color: "rgb(241, 241, 241)", cursor: "pointer", maxWidth:"max-content"}}>See all comments →</Comment.Text>
-    : null}
-    <UserRating rank={rank} setRank={setRank} />
-    <Form onSubmit={handleSubmit}>
-      <Form.TextArea className={styles.textarea_styling} style={{ minWidth: "500px", minHeight: "200px", borderRadius: "5px", padding: '10px 10px 10px 10px', fontSize: "1em", resize: "none" }} placeholder='Tell us more' onInput={(event)=>setText(event.target.value)} value={text} />
-      <Button disabled={text.length<4} style={{color:"#FFFFFF", background: "linear-gradient(114.93deg, #618967 5.11%, #72a479 94%)", padding: '10px 10px 10px 10px', marginTop: "10px", border: "none", borderRadius:"5px", cursor: "pointer", fontSize: "18px"}} onClick={()=>{addComment(id,text)}} labelPosition='left' primary><FontAwesomeIcon icon={faPencilAlt}/> Add Comment </Button>
-    </Form>
-  </Comment.Group>
-  
-  )
+    <Comment.Group style={{ paddingTop: "20px" }}>
+      <Comment style={{ padding: "10px 0px 10px 0px", maxWidth: "500px" }}>
+        {commentsFilter.length > 0 ? (
+          comm.map((elem) => (
+            <UserComment
+              key={elem._id}
+              commentsId={elem._id}
+              firstName={elem.customer.firstName}
+              content={elem.content}
+              date={elem.datePublic}
+              avatar={elem.customer.avatarUrl}
+              rating={elem.rating}
+              thumbUpUser={elem.thumbUp === undefined ? 0 : elem.thumbUp}
+              thumbDownUser={elem.thumbDown === undefined ? 0 : elem.thumbDown}
+            />
+          ))
+        ) : (
+          <p
+            style={{
+              fontSize: "18px",
+              color: "rgb(241, 241, 241)",
+              padding: "20px 0px 20px 20px",
+              backgroundColor: "#4e4948",
+              borderRadius: "10px",
+            }}
+          >
+            No comments
+          </p>
+        )}
+      </Comment>
+      {commentsFilter.length > 5 && visibleMore ? (
+        <Comment.Text
+          onClick={handleShowMoreComments}
+          style={{
+            marginBottom: "20px",
+            color: "rgb(241, 241, 241)",
+            cursor: "pointer",
+            maxWidth: "max-content",
+          }}
+        >
+          See all comments →
+        </Comment.Text>
+      ) : null}
+      <UserRating rank={rank} setRank={setRank} />
+      <Form onSubmit={handleSubmit}>
+        <Form.TextArea
+          className={styles.textarea_styling}
+          style={{
+            minWidth: "500px",
+            minHeight: "200px",
+            borderRadius: "5px",
+            padding: "10px 10px 10px 10px",
+            fontSize: "1em",
+            resize: "none",
+          }}
+          placeholder="Tell us more"
+          onInput={(event) => setText(event.target.value)}
+          value={text}
+        />
+        <Button
+          disabled={text.length < 4}
+          style={{
+            color: "#FFFFFF",
+            background:
+              "linear-gradient(114.93deg, #618967 5.11%, #72a479 94%)",
+            padding: "10px 10px 10px 10px",
+            marginTop: "10px",
+            border: "none",
+            borderRadius: "5px",
+            cursor: "pointer",
+            fontSize: "18px",
+          }}
+          onClick={() => {
+            addComment(id, text);
+          }}
+          labelPosition="left"
+          primary
+        >
+          <FontAwesomeIcon icon={faPencilAlt} /> Add Comment{" "}
+        </Button>
+      </Form>
+    </Comment.Group>
+  );
 }
 
 Comments.propTypes = {
-    id: PropTypes.string,
-    flag: PropTypes.bool.isRequired,
-    setFlag: PropTypes.func.isRequired,
-}
+  id: PropTypes.string,
+  flag: PropTypes.bool.isRequired,
+  setFlag: PropTypes.func.isRequired,
+};
 
-Comments.defaultProps={
+Comments.defaultProps = {
   id: "",
-}
+};
 
 export default Comments;
--- src/components/Find/CheckBoxContainer/CheckBoxContainer.js
+++ src/components/Find/CheckBoxContainer/CheckBoxContainer.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-bind */
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { initCategories } from "../../../store/actionCreators/cardItemsCreator";
@@ -6,17 +7,16 @@ import style from "./CheckBoxContainer.module.scss";
 
 function CheckBoxContainer() {
   const categories = useSelector((store) => store.items.categories);
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  useEffect(() => {
-  }, [categories]);
+  useEffect(() => {}, [categories]);
 
-  const setChecked = (id)=>{
-    
-    if(categories.includes(id)){
-        return true
-    } else {return false}
-  } 
+  const setChecked = (id) => {
+    if (categories.includes(id)) {
+      return true;
+    }
+    return false;
+  };
 
   function change(e) {
     const { value } = e.target;
@@ -26,17 +26,45 @@ function CheckBoxContainer() {
   return (
     <div className={style.checkBox_container}>
       <form className={style.checkBox_form}>
-      <CheckBox id='cold snaks' name={'Cold snaks'} funcOnChange={change} funChecked={setChecked}/>
-      <CheckBox id='soup' name={'Soup'} funcOnChange={change} funChecked={setChecked}/>
-      <CheckBox id='salads' name={'Salads'} funcOnChange={change} funChecked={setChecked}/>
-      <CheckBox id='main dishes' name={'Main dishes'} funcOnChange={change} funChecked={setChecked}/>
-      <CheckBox id='desert' name={'Deserts'} funcOnChange={change} funChecked={setChecked}/>
-      <CheckBox id='hot snaks' name={'Hot snaks'} funcOnChange={change} funChecked={setChecked}/>
+        <CheckBox
+          id="cold snaks"
+          name="Cold snaks"
+          funcOnChange={change}
+          funChecked={setChecked}
+        />
+        <CheckBox
+          id="soup"
+          name="Soup"
+          funcOnChange={change}
+          funChecked={setChecked}
+        />
+        <CheckBox
+          id="salads"
+          name="Salads"
+          funcOnChange={change}
+          funChecked={setChecked}
+        />
+        <CheckBox
+          id="main dishes"
+          name="Main dishes"
+          funcOnChange={change}
+          funChecked={setChecked}
+        />
+        <CheckBox
+          id="desert"
+          name="Deserts"
+          funcOnChange={change}
+          funChecked={setChecked}
+        />
+        <CheckBox
+          id="hot snaks"
+          name="Hot snaks"
+          funcOnChange={change}
+          funChecked={setChecked}
+        />
       </form>
     </div>
   );
 }
-
-
 
 export default CheckBoxContainer;
